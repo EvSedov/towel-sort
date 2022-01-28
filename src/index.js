e1c0b3f5a58e84f5730ff137e18d69726cc535ca
Evgeniy Sedov <sedoff-online@yandex.ru>
2022-01-28 20:09:08 +0700

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let result = [];
+  if (matrix == undefined) {
+    return result;
+  }
+  matrix.forEach((el, idx) => {
+    const currentElement = (idx % 2 == 0) ? el : el.reverse();
+    result = [...result, ...currentElement];
+  })
+  return result;
 }
